9420c07fbfe4b937899a6808f0b24f8431b7d678
mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-11-14 17:04:00 +0100

    Changed bucket name to be defined by a parameter

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -16,8 +16,8 @@ s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
 # Oppgave 1A
-BUCKET_NAME = "rekognition-2015"
-print(os.environ.get("BUCKET_NAME", "no env found"))
+print("Env is " + os.environ.get("BUCKET_NAME", "no env found"))
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
 
 def lambda_handler(event, context):
 
