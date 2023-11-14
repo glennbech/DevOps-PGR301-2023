aac06aad81dea63e0238791a17a87b2011c471da
mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-11-14 15:17:23 +0100

    Tried with own hardcoded values

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -16,7 +16,8 @@ s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
 # Oppgave 1A
-BUCKET_NAME = "kjellsimagebucker"
+BUCKET_NAME = "rekognition-2015"
+print(os.environ.get("BUCKET_NAME", "no env found"))
 
 def lambda_handler(event, context):
 
