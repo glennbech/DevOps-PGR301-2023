7de4bc9304f394629e352262748d66ae8332ee91
mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-11-17 12:45:43 +0100

    Refactored out hardcoding in infra code

---
+++ infra/variables.tf
@@ -0,0 +1,23 @@
+variable "service_name" {
+  type = string
+  description = "The name of this service"
+}
+
+variable "aws_account_number" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+  description = "The region of the ecr repository"
+}
+
+variable "ecr_repository" {
+  type = string
+  description = "the name of the ecr repository"
+}
+
+variable "ecr_tag" {
+  type = string
+  default = "latest"
+}
